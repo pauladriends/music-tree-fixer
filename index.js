e77e3c1e0eac35e876c026ea0cdef8013efb3425
Paul-Adrien DOUTEAU-SALA <paul.douteau@soregies.fr>
2023-11-16 17:09:22 +0100

    Update index.js and package.json

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const { Command } = require("commander");
 const program = new Command();
-const dree = require("dree");
 const packageJson = require("./package.json");
+const dree = require("dree");
+
+function scanArtist(directory) {
+  let scanResult = [];
+  dree.scan(directory).children.forEach((artist) => {
+    item = {};
+    item.artist = artist.name;
+    item.tracks = [];
+    artist.children.map((track) => {
+      track = {};
+      track.fullname = track.name;
+      item.tracks.push(track);
+    });
+    scanResult.push(item);
+  });
+  return scanResult;
+}
+
+function splitTrack(trackFullname) {
+  splittedTrack = trackFullname.split(" - ");
+  return {
+    artist: splittedTrack[0],
+    name: splitTrack[1],
+  };
+}
 
 program
   .name(packageJson.name)
   .description(packageJson.description)
   .version(packageJson.version);
+
+program
+  .command("scan")
+  .description("List all anomalies")
+  .argument("<directory>", "Your Music Directory to read")
+  .action((directory) => {
+    scanArtist(directory).forEach((artist) => {
+      console.log(
+        `${artist.name}`,
+        artist.children.map((music) => music.name)
+      );
+  });
 
 program
   .command("tree")
